cd0674f603e3e6d2b9646a8aa4d59484513f2b98
OgorodnikSergey <osv8487@gmail.com>
2019-06-21 19:18:47 +0300

    HomeWork412v1

--- app/src/main/java/ru/ogorodnik/homework412/MainActivity.java
+++ app/src/main/java/ru/ogorodnik/homework412/MainActivity.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ListView myList;
+    final List<Map<String, String>> content = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +25,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myList = findViewById(R.id.myList_view);
-
-        final List<Map<String, String>> content = new ArrayList<>();
-
-        Map<String, String> firstRowMap = new HashMap<>();
-        firstRowMap.put("upper_text", getString(R.string.text1));
-        //int text =  (getString(R.string.text1)).length(); Оставил эту строку для себя в качетсве напоминания
-        firstRowMap.put("down_text", String.valueOf((getString(R.string.text1)).length()));
-        content.add(firstRowMap);
-
-        Map<String, String> secondRowMap = new HashMap<>();
-        secondRowMap.put("upper_text", getString(R.string.text2));
-        secondRowMap.put("down_text", String.valueOf((getString(R.string.text2)).length()));
-        content.add(secondRowMap);
+        updateList();
 
         final SimpleAdapter adapter = new SimpleAdapter(this, content, R.layout.activity_main,
                 new String[]{ "upper_text", "down_text" }, new int[]{ R.id.upper_text, R.id.down_text });
         myList.setAdapter(adapter);
 
-        final SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
+         final SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
         swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             // Будет вызван, когда пользователь потянет список вниз
             @Override
             public void onRefresh() {
-                //updateList();
+
+                content.clear();
+                updateList();
+                adapter.notifyDataSetChanged();
                 swipeLayout.setRefreshing(false);
             }
         });
@@ -65,6 +57,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
+    public void updateList(){
+
+        Map<String, String> firstRowMap = new HashMap<>();
+        firstRowMap.put("upper_text", getString(R.string.text1));
+        //int text =  (getString(R.string.text1)).length(); Оставил эту строку для себя в качетсве напоминания
+        firstRowMap.put("down_text", String.valueOf((getString(R.string.text1)).length()));
+        content.add(firstRowMap);
+
+        Map<String, String> secondRowMap = new HashMap<>();
+        secondRowMap.put("upper_text", getString(R.string.text2));
+        secondRowMap.put("down_text", String.valueOf((getString(R.string.text2)).length()));
+        content.add(secondRowMap);
+    }
+
 }
